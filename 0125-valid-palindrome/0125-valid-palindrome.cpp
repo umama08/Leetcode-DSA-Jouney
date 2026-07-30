da718c31fef32b261d7e080a05173c6722be7db5
umama ahmed <umamaahmed08@gmail.com>
2026-07-31 04:43:45 +0500

    Time: 3 ms (28.4%), Space: 10 MB (54.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
  
- bool isAlphanum(char ch){
-    if((ch>='0' && ch <='9') || (tolower(ch))>='a' && (tolower(ch)<='z' )){
-        return true;
-    }
-    return false;
- }
+ 
     bool isPalindrome(string s) {
         int st=0,end=s.length()-1;
         while(st<end){
-        if(!isAlphanum(s[st])){
+        if(!isalnum(s[st])){
             st++;
             continue;
         }
-        if(!isAlphanum(s[end])){
+        if(!isalnum(s[end])){
             end--;
             continue;
         }
